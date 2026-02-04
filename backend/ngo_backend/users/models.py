78b0f0b47ee82ea93ca15f2c8f6c84c578a401bd
Yash Ashture <yashashture2@gmail.com>
2026-02-04 17:13:15 +0530

    Day 04: Profile model added and registered in admin

--- backend/ngo_backend/users/models.py
+++ backend/ngo_backend/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=100)
+    role = models.CharField(max_length=50, default='user')
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.user.username
