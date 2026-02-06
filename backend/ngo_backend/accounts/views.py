d1579721100175456921a5a22ba73f10760b7ad1
Yash Ashture <yashashture2@gmail.com>
2026-02-06 14:31:36 +0530

    Day 05: Registration and Login system

--- backend/ngo_backend/accounts/views.py
+++ backend/ngo_backend/accounts/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Create your views here.
+def register(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        full_name = request.POST.get("full_name")
+
+        user = User.objects.create_user(
+            username=username,
+            password=password
+        )
+        Profile.objects.create(
+            user=user,
+            full_name=full_name
+        )
+
+        return redirect("/admin/")
+
+    return render(request, "register.html")
+from django.contrib.auth import authenticate, login, logout
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("/admin/")
+        else:
+            return render(request, "login.html", {"error": "Invalid username or password"})
+
+    return render(request, "login.html")
+
+
+def user_logout(request):
+    logout(request)
+    return redirect("/login/")
