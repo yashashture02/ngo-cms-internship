03bd736e364382ca88578bf54801e82f9c8aae97
Yash Ashture <yashashture2@gmail.com>
2026-03-27 18:30:37 +0530

    Final fix: homepage route for health check

--- events/urls.py
+++ events/urls.py
@@ -2,7 +2,7 @@ from django.urls import path # type: ignore
 from . import views
 
 urlpatterns = [
-    path('', views.home),                # homepage
+    path('', views.home, name='home'),                # homepage
     path('events/', views.event_list),  # list
     path('add/', views.add_event),      # add
     path('edit/<int:id>/', views.edit_event),
